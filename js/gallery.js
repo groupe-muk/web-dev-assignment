5ea3ef50564566e8d99fd58f5a3bec0d17626931
Sunshine757 <nabwamilydia5@gmail.com>
2025-04-01 23:14:33 +0300

    fixed the lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,20 @@
+console.log("gallery.js loaded successfully!");
+
+
 const lightbox=document.createElement('div');
 lightbox. id ='lightbox';
 document.body.appendChild(lightbox);
-const images =document.querySelectorAll('img');
+const images =document.querySelectorAll('.flex-container img');
+console.log("Number of images found:",images.length);
 images.forEach(image =>{
     image.addEventListener('click', (e)=>{
+        console.log("Image clicked:", e. target.src);
         lightbox.classList.add('active');
+        console.log("Lightbox activated");
         const img =document.createElement('img');
         img.src = image.src;
-        while(lightbox.firstchild){
-            lightbox.removeChild(lightbox.firstchild);
+        while(lightbox.firstChild){
+            lightbox.removeChild(lightbox.firstChild);
         }
 
         lightbox.appendChild(img);
@@ -20,7 +26,7 @@ images.forEach(image =>{
 
 )
 lightbox.addEventListener('click', (e)=>{
-    if(e.target!==e.currenttarget) return
+    if(e.target!==e.currentTarget) return
     lightbox.classList.remove('active');
 
 }
