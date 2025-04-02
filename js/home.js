7397f835c42dd91561a83d8c27e1019bd84270de
Magezi-Eunice <eunicjasmine84@gmail.com>
2025-04-02 12:53:15 +0300

    Made testimonial section responsive

--- js/home.js
+++ js/home.js
@@ -36,3 +36,51 @@ function prevSlide(){
 let autoSlide = setInterval(nextSlide, 3000);
 
 updateBackground();
+
+let index = 0;
+
+const testimonials = document.querySelectorAll('.testimonial');
+const totalTestimonials = testimonials.length;
+
+if(testimonials.length > 0){
+    testimonials[0].classList.add("active");
+}
+
+
+function showTestimonial(){
+    testimonials.forEach((testimonial) =>{
+        testimonial.classList.remove("active");
+        testimonial.style.display = "none";
+    });
+    testimonials[index].classList.add("active");
+    testimonials[index].style.display = "block";
+}
+
+function nextTestimonial(){
+    index = (index + 1)% testimonials.length;
+    showTestimonial();
+}
+
+function prevTestimonial(){
+    index = (index - 1 + testimonials.length)% testimonials.length;
+    showTestimonial();
+}
+
+function autoMove(){
+    nextTestimonial();
+}
+
+let autoMoveInterval = setInterval(autoMove,3000);
+
+
+document.querySelector('.testimonial-carousel').addEventListener('mouseover', () => {
+    clearInterval(autoMoveInterval);
+});
+
+// Resume auto-slide on mouse leave
+document.querySelector('.testimonial-carousel').addEventListener('mouseleave', () => {
+    autoMoveInterval = setInterval(autoMove, 3000);
+});
+
+document.getElementById("prevbtn").addEventListener("click", prevTestimonial);
+document.getElementById("nextbtn").addEventListener("click", nextTestimonial);
